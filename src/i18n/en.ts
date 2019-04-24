f6198a7c92dec843f34a45b51df0addb1f0e8c2b
ColMugX <colmugx@qq.com>
2019-04-24 16:32:34 +0800

    add: i18n.

---
+++ src/i18n/en.ts
@@ -0,0 +1,20 @@
+export default {
+  'info.profile.name': 'Name: ',
+  'info.profile.job': 'Job: ',
+  'info.profile.base': 'Base: ',
+  'info.profile.local': 'Local: ',
+  'info.profile.phone': 'Phone: ',
+  'info.profile.homepage': 'HomePage: ',
+  'info.profile.github': 'Github: ',
+  'info.profile.email': 'Email: ',
+  'info.profile.qq': 'QQ: ',
+  'info.profile.tags': 'Tags: ',
+  'info.others.educations': 'Educations',
+  'info.others.skills': 'Skills',
+  'info.others.experiences': 'Experiences',
+  'info.others.projects': 'Project',
+  'info.others.personal': 'Personal',
+
+  // Common Message
+  'common.editor': 'Editor',
+};
---
+++ src/i18n/zh_cn.ts
@@ -0,0 +1,21 @@
+export default {
+  // info
+  'info.profile.name': '姓名：',
+  'info.profile.job': '应聘岗位：',
+  'info.profile.base': '期望地区：',
+  'info.profile.local': '当前在：',
+  'info.profile.phone': '联系电话：',
+  'info.profile.homepage': '个人主页：',
+  'info.profile.github': 'Github：',
+  'info.profile.email': 'Email：',
+  'info.profile.qq': 'QQ：',
+  'info.profile.tags': '关键词：',
+  'info.others.educations': '教育经历',
+  'info.others.skills': '技能列表',
+  'info.others.experiences': '工作经历',
+  'info.others.projects': '公开项目',
+  'info.others.personal': '其他信息',
+
+  // Common Message
+  'common.editor': '编辑器',
+};
