c98b0605cc7781a5e9508de851605017216e2477
ColMugX <colmugx@qq.com>
2019-04-24 16:32:12 +0800

    add: umi.

---
+++ .umirc.ts
@@ -0,0 +1,31 @@
+import { IConfig } from 'umi-types';
+
+// ref: https://umijs.org/config/
+const config: IConfig =  {
+  treeShaking: true,
+  plugins: [
+    // ref: https://umijs.org/plugin/umi-plugin-react.html
+    ['umi-plugin-react', {
+      antd: true,
+      dva: true,
+      dynamicImport: { webpackChunkName: true },
+      title: 'resume',
+      dll: true,
+      sass: {},
+      routes: {
+        exclude: [
+          /models\//,
+          /services\//,
+          /model\.(t|j)sx?$/,
+          /service\.(t|j)sx?$/,
+          /components\//,
+        ],
+      },
+    }],
+  ],
+  theme: {
+    "primary-color": "#999"
+  },
+}
+
+export default config;
