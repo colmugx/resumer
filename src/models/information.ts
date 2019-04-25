f52e455902bf67d59cad8c3a9c56f6512619643b
ColMugX <colmugx@qq.com>
2019-04-25 22:42:51 +0800

    update: model - information.

--- src/models/information.ts
+++ src/models/information.ts
@@ -1,17 +1,34 @@
 export default {
   state: {
-    profile: {
-    },
-    others: {
-    },
+    profile: {},
+    others: {},
   },
   reducers: {
-    update(state: any, { type, data }: any) {
+    update(state: any, { payload: { type, data } }: any) {
       return {
         ...state,
         [type]: data,
       };
     },
   },
-  effects: {},
+  effects: {
+    *updateProfile({ payload }: any, { put }: any) {
+      yield put({
+        type: 'update',
+        payload: {
+          type: 'profile',
+          data: payload,
+        },
+      });
+    },
+    *updateOthers({ payload }: any, { put }: any) {
+      yield put({
+        type: 'update',
+        payload: {
+          type: 'others',
+          data: payload,
+        },
+      });
+    },
+  },
 };
